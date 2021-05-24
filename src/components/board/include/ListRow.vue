3032d19e755b5002b84ddf8da39aae16c80ad12d
ParkSeRyeong <63963540+ParkSeRyeong@users.noreply.github.com>
2021-05-24 11:16:47 +0900

    ListRow 수정했오요

--- src/components/board/include/ListRow.vue
+++ src/components/board/include/ListRow.vue
@@ -2,11 +2,7 @@
   <tr>
     <td>{{ no }}</td>
     <td>
-<<<<<<< HEAD
-      <router-link :to="`Board/view?no=${no}`">{{ title }}</router-link>
-=======
       <router-link :to="`board/view?no=${no}`">{{ title }}</router-link>
->>>>>>> cf8fea91b7520c541d40afcbaabff99f9e283532
     </td>
     <td>{{ writer }}</td>
     <td>{{ getFormatDate(regtime) }}</td>
@@ -14,29 +10,27 @@
 </template>
 
 <script>
-
 import moment from 'moment';
 
 export default {
-  name: "ListRow",
+  name: 'ListRow',
   props: {
     no: Number,
     title: String,
 
     writer: String,
-    content:String,
-    regtime: String
+    content: String,
+    regtime: String,
   },
   methods: {
     getFormatDate(regtime) {
       return moment(new Date(regtime)).format('YYYY.MM.DD');
     },
-  }
+  },
 };
 </script>
 
 <style>
-
 td {
   text-align: center;
   border-bottom: 1px solid #ddd;
